392cc64deb5ee3e94ede040f329423acd226a536
=ari <=ahshari2003@gmail.com>
2021-07-06 07:56:48 +0530

    working on sidebar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./App.css"
+import Sidebar from './Sidebar'
+function App() {
+  return (
+    <div className="app">
+     <div className="app__body">
+    <Sidebar/>
+     </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Sidebar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Sidebar.css"
+function Sidebar() {
+    return (
+        <div className='sidebar'>
+            <div clasName="sidebar">
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
